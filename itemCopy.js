8a18cdb1a3990e583025f291c26c57c6dc49d0ed
Clay Diffrient <clay.diffrient@gmail.com>
2012-02-07 12:06:12 -0700

    Bug correction

--- itemCopy.js
+++ itemCopy.js
@@ -220,7 +220,7 @@ function getCourseItemsLeft()
                                     }
 
                                     
-                                 }
+                                 });
                                  
                                  $(leftCourseXML).find("item data").each(function()
                                  {
